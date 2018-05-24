9b98f54c8a9c260b16235410bde99b85f5b4bdbe
Rahul <Rahul@DESKTOP-GSBND7Q>
2018-05-25 02:36:37 +0530

    Implemented Exception handling..

--- src/main/java/com/rest/demo/controller/UserController.java
+++ src/main/java/com/rest/demo/controller/UserController.java
@@ -30,7 +30,11 @@ public class UserController {
 	
 	@GetMapping("/users/{id}")
 	public User retriveUserById(@PathVariable int id) {
-		return service.findOne(id);
+				User user = service.findOne(id);
+				if(user==null)
+					throw new UserNotFoundException("id-"+id);
+				
+				return user;
 	}
 	
 	@PostMapping("/users")
